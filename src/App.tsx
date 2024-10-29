f0dec98b1d5ab2870c4a6fb184447815faddd534
Andrei Lihtgolt <andrei.lihtgolt@actionengine.com>
2024-10-29 13:22:59 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import { Home } from "./atoms/Home";
+
+// router
+// Context
+//
+
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* <ContextProvider>
+        <BrowserRouter>
+          <ProtectedRoutes>
+            <Main />
+          </ProtectedRoutes>
+          <SignIn />
+          <Registration />
+        </BrowserRouter>
+      </ContextProvider> */}
+    </>
+  );
 }
+
+// function App() {
+//   const [state, setState] = useState(0);
+//   const handleClick = () => {
+//     setState(0);
+//   };
+//   const handleClickOther = () => {
+//     setState(1);
+//   };
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         <Home state={state == 0} />
+//         <span>Home</span>
+//       </button>
+//       <button onClick={handleClickOther}>
+//         <Home state={state == 1} />
+//         <span>Other Home</span>
+//       </button>
+//     </>
+//   );
+// }
 
 export default App;
