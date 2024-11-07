602b03d6d164aa29d6d1ace47c2f9d3b40b466fe
Andrei Lihtgolt <andrei.lihtgolt@actionengine.com>
2024-11-07 13:18:31 +0200

    some features

--- src/molecules/PostsList.tsx
+++ src/molecules/PostsList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { $api } from "../api/api";
+import styles from "./Posts.module.css";
+import { parseData } from "../helpers/parseData";
 
 type Post = {
   _id: string;
@@ -19,29 +21,84 @@ export const PostsList = () => {
     const getPosts = () => {
       $api.get("/post/all").then((res) => setPosts(res.data));
     };
+    const getFollowers = () => {
+      $api.get(`/follow/672c78f0b6361b9bb37beca9/followers`);
+    };
+    getFollowers();
     getPosts();
   }, []);
 
   return (
-    <ul>
+    <ul className={styles.postList}>
       {posts.length > 0 ? (
         posts.map((item: Post) => (
-          <PostItem img={item.image_url} caption={item.caption} />
+          <PostItem key={item._id} item={item} isFollow={true} />
         ))
       ) : (
-        <span>No Posts</span>
+        <li>No Posts</li>
       )}
     </ul>
   );
 };
 
-const PostItem = ({ caption, img }: { caption: string; img: string }) => {
+type ItemProps = {
+  item: {
+    caption: string;
+    created_at: string;
+    image_url: string;
+    profile_image: string;
+    user_name: string;
+    user_id: string;
+    likes_count: number;
+  };
+  isFollow: boolean;
+};
+
+const PostItem = ({ item, isFollow }: ItemProps) => {
+  const [userName, setUsername] = useState("");
+  // const isFollow = [id1, id2, id3].find(it => it === item.user_id) ? true : false
+  // useEffect(() => {
+  //   const response = $api.get('/')
+  // },[])
+
   return (
-    <li>
-      <img src={img} alt="" />
-      {caption}
+    <li className={styles.postItem}>
+      <div>
+        <img src={item.profile_image} alt="avatar" />
+        <span>{item.user_name}</span>
+        <span className={styles.created}>
+          &#8226;{parseData(item.created_at)}&#8226;
+        </span>
+        <FollowButton
+          isFollow={isFollow}
+          userId={item.user_id} // To-Do
+          targetUserId={item.user_id}
+        />
+      </div>
+      <img src={item.image_url} alt="" />
+      {item.caption}
     </li>
   );
 };
+
+interface FollowButtonProps {
+  isFollow: boolean;
+  userId: string;
+  targetUserId: string;
+}
+
+function FollowButton({ isFollow, userId, targetUserId }: FollowButtonProps) {
+  const handleFollow = () => {
+    if (!isFollow) {
+      $api.post(`/follow/${userId}/follow/${targetUserId}`);
+    } else {
+      $api.delete(`/follow/${userId}/unfollow/${targetUserId}`);
+    }
+  };
+
+  return (
+    <button onClick={handleFollow}>{isFollow ? "follow" : "unfollow"}</button>
+  );
+}
 
 export default PostsList;
